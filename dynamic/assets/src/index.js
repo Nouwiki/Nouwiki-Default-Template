578f6ba75b0acc434b1c9dd6748e7af1e882cd30
Brynjar Harðarson <brynjar@gmail.com>
2016-01-15 21:28:39 +0000

    nouwiki template init

---
+++ dynamic/assets/src/index.js
@@ -0,0 +1,6 @@
+/* CSS */
+require("./css/prose-bright.css");
+require("./css/ui.css");
+
+/* JS */
+require("./js/ui.js");
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+  node: {
+    fs: "empty"
+  },
+  entry: './dynamic/assets/src/index.js',
+  output: {
+    filename: './dynamic/assets/js/ui.js'
+  },
+  module: {
+    //noParse: [/autoit.js/],
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader?sourceMap") },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader') }, // use ! to chain loaders
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      { test: /\.(png|jpg)$/, loapackder: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
+      { test: /\.json$/, loader: "json" }
+    ]
+  },
+  plugins: [
+      new ExtractTextPlugin("./dynamic/assets/css/ui.css")
+  ]
+};
